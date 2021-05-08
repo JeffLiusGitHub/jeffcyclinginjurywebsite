31689967f690b383a4c87b65b32a137a023fc821
jeffliu <zliu0054@student.monash.edu>
2021-05-08 22:23:59 +1000

    new file:   Controllers/QuizIndexController.cs git remote add origin https://github.com/JeffLiusGitHub/jeffcyclinginjurywebsite.git#

---
+++ Controllers/EducationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ie_sport_injury_ver_1._3.Controllers
+{
+    public class EducationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+
+        public ActionResult EssentialRoadRules()
+        {
+            return View();
+        }
+
+        public ActionResult RidingInTraffic()
+        {
+            return View();
+        }
+
+        public ActionResult RidingLanesAndPaths()
+        {
+            return View();
+        }
+
+        public ActionResult BikeComponents()
+        {
+            return View();
+        }
+
+        public ActionResult WearingHelmet()
+        {
+            return View();
+        }
+
+    }
+}
